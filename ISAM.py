3eabf25f1cf6c7e7f0263c1f23fe083c0c0692a4
ariana.mercadoutec.edu.pe <ariana.mercado@utec.edu.pe>
2025-10-19 18:22:11 -0500

    ISAM pruebas completas?

--- ISAM.py
+++ ISAM.py
@@ -1,142 +1,347 @@
 import os
 import struct
+from fileinput import filename
 
 BLOCK_FACTOR = 3
 INDEX_BLOCK_FACTOR = 3
 
-DATA_FILE = 'data_isam.bin'
-INDEX_FILE = 'index_isam.bin'
-
-RECORD_FORMAT = 'i'
-RECORD_SIZE = struct.calcsize(RECORD_FORMAT)
-
 class Record:
-    def __init__(self, id_):
+    FORMAT = 'i'
+    SIZE_OF_RECORD = struct.calcsize(FORMAT)
+    def __init__(self, id_: int):
         self.id = id_
 
-    def pack(self):
-        return struct.pack(RECORD_FORMAT, self.id)
+    def pack(self) -> bytes:
+        return struct.pack(
+            self.FORMAT,
+            self.id
+        )
 
     @staticmethod
     def unpack(data):
-        id_, = struct.unpack(RECORD_FORMAT, data)
+        id_, = struct.unpack(Record.FORMAT, data)
         return Record(id_)
 
     def __repr__(self):
         return f"Record(id={self.id})"
 
 class DataPage:
-    def __init__(self, records=None, overflow=None):
-        self.records = records or []
-        self.overflow = overflow
+    HEADER_FORMAT = 'ii'
+    HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+    SIZE_OF_PAGE = HEADER_SIZE + BLOCK_FACTOR * Record.SIZE_OF_RECORD
+
+    def __init__(self, records=None, next_page=-1):
+        self.records = list(records) if records else []
+        self.next_page = next_page
+
+    def has_space(self):
+        return len(self.records) < BLOCK_FACTOR
 
     def is_full(self):
         return len(self.records) >= BLOCK_FACTOR
 
-    def add_record(self, record):
-        self.records.append(record)
-        self.records.sort(key=lambda r: r.id)
+    def pack(self) -> bytes:
+        header_data = struct.pack(self.HEADER_FORMAT, len(self.records), self.next_page)
+        records_data = b''.join(r.pack() for r in self.records)
+        pad = BLOCK_FACTOR - len(self.records)
+        if pad > 0:
+            records_data += b'\x00' * (pad * Record.SIZE_OF_RECORD)
+        return header_data + records_data
 
-    def __repr__(self):
-        ids = [r.id for r in self.records]
-        return f"DataPage(records={ids}, overflow={self.overflow})"
+    @staticmethod
+    def unpack(data: bytes):
+        size, next_page = struct.unpack(DataPage.HEADER_FORMAT, data[:DataPage.HEADER_SIZE])
+        records = []
+        offset = DataPage.HEADER_SIZE
+        for _ in range(size):
+            chunk = data[offset: offset + Record.SIZE_OF_RECORD]
+            records.append(Record.unpack(chunk))
+            offset += Record.SIZE_OF_RECORD
+        return DataPage(records, next_page)
 
 class ISAMFile:
-    def __init__(self):
-        self.data_pages = []
-        self.index_level_1 = []
-        self.index_root = []
+    INDEX_ENTRY_FMT = 'ii'
+    INDEX_ENTRY_SIZE = struct.calcsize(INDEX_ENTRY_FMT)
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.filename_idx = filename + '_idx'
+        self.leaf = []
+        self.root = []
+        self.primary_page_count = 0
+
+    @staticmethod
+    def _first_ge_index(keys, key):
+        for i, v in enumerate(keys):
+            if v >= key:
+                return i
+        return len(keys)
+
+    def _file_size(self):
+        return os.path.getsize(self.filename) if os.path.exists(self.filename) else 0
+
+    def _total_pages(self):
+        return self._file_size() // DataPage.SIZE_OF_PAGE
+
+    def _read_page(self, f, page_no) -> DataPage:
+        f.seek(page_no * DataPage.SIZE_OF_PAGE)
+        data = f.read(DataPage.SIZE_OF_PAGE)
+        return DataPage.unpack(data)
+
+    def _write_page(self, f, page_no, page: DataPage):
+        f.seek(page_no * DataPage.SIZE_OF_PAGE)
+        f.write(page.pack())
+
+    def _append_page(self, f, page: DataPage) -> int:
+        f.seek(0, os.SEEK_END)
+        pos = f.tell()
+        f.write(page.pack())
+        return pos // DataPage.SIZE_OF_PAGE
+
+    def _save_index(self):
+        with open(self.filename_idx, 'wb') as f:
+            f.write(struct.pack('i', len(self.leaf)))
+            for mx, pno in self.leaf:
+                f.write(struct.pack(self.INDEX_ENTRY_FMT, mx, pno))
+            f.write(struct.pack('i', len(self.root)))
+            for mx, start in self.root:
+                f.write(struct.pack(self.INDEX_ENTRY_FMT, mx, start))
+
+    def _load_index(self):
+        self.leaf, self.root = [], []
+        if not os.path.exists(self.filename_idx):
+            self.primary_page_count = 0
+            return
+        with open(self.filename_idx, 'rb') as f:
+            b = f.read(4)
+            if not b:
+                self.primary_page_count = 0
+                return
+            n_leaf = struct.unpack('i', b)[0]
+            for _ in range(n_leaf):
+                mx, pno = struct.unpack(self.INDEX_ENTRY_FMT, f.read(self.INDEX_ENTRY_SIZE))
+                self.leaf.append((mx, pno))
+            n_root_b = f.read(4)
+            if not n_root_b:
+                return
+            n_root = struct.unpack('i', n_root_b)[0]
+            for _ in range(n_root):
+                mx, start = struct.unpack(self.INDEX_ENTRY_FMT, f.read(self.INDEX_ENTRY_SIZE))
+                self.root.append((mx, start))
+        self.primary_page_count = len(self.leaf)
+
+    def build_index(self):
+        self.leaf, self.root = [], []
+        if not os.path.exists(self.filename):
+            self._save_index()
+            return
+        total = self._total_pages()
+        with open(self.filename, 'rb') as f:
+            for i in range(total):
+                page = self._read_page(f, i)
+                if len(page.records) == 0:
+                    break
+                max_key = max(r.id for r in page.records)
+                self.leaf.append((max_key, i))
+        self.primary_page_count = len(self.leaf)
+        for i in range(0, len(self.leaf), INDEX_BLOCK_FACTOR):
+            bloque = self.leaf[i:i + INDEX_BLOCK_FACTOR]
+            max_key = bloque[-1][0]
+            self.root.append((max_key, i))
+        self._save_index()
+
+    def _root_find_block(self, key):
+        if not self.root:
+            return None
+        keys = [mx for mx, _ in self.root]
+        i = self._first_ge_index(keys, key)
+        return self.root[i][1] if i < len(self.root) else None
+
+    def _leaf_find_primary(self, start_idx, key):
+        sub = self.leaf[start_idx:start_idx + INDEX_BLOCK_FACTOR]
+        if not sub:
+            return None
+        keys = [mx for mx, _ in sub]
+        j = self._first_ge_index(keys, key)
+        return sub[j][1] if j < len(sub) else sub[-1][1]
+
+    def _find_primary_page_for_key(self, key):
+        self._load_index()
+        if not self.leaf:
+            return None
+        start = self._root_find_block(key)
+        if start is None:
+            return self.leaf[-1][1]
+        return self._leaf_find_primary(start, key)
 
     def build_from_records(self, records):
-        records.sort(key=lambda r: r.id)
-        for i in range(0, len(records), BLOCK_FACTOR):
-            page_records = records[i:i + BLOCK_FACTOR]
-            page = DataPage(page_records)
-            self.data_pages.append(page)
-        for i, page in enumerate(self.data_pages):
-            max_key = max(r.id for r in page.records)
-            self.index_level_1.append((max_key, i))
-        for i in range(0, len(self.index_level_1), INDEX_BLOCK_FACTOR):
-            chunk = self.index_level_1[i:i + INDEX_BLOCK_FACTOR]
-            max_key = chunk[-1][0]
-            self.index_root.append((max_key, i))
-        self.save_to_disk()
+        recs = sorted(records, key=lambda r: r.id)
+        with open(self.filename, 'wb') as f:
+            for i in range(0, len(recs), BLOCK_FACTOR):
+                chunk = recs[i:i + BLOCK_FACTOR]
+                page = DataPage(chunk, -1)
+                self._append_page(f, page)
+        self.build_index()
 
-    def save_to_disk(self):
-        with open(DATA_FILE, 'wb') as df:
-            for page in self.data_pages:
-                for r in page.records:
-                    df.write(r.pack())
-        with open(INDEX_FILE, 'wb') as idxf:
-            for key, page_index in self.index_level_1:
-                idxf.write(struct.pack('ii', key, page_index))
-            for key, page_index in self.index_root:
-                idxf.write(struct.pack('ii', key, page_index))
+    def add(self, record: Record):
+        self._load_index()
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'wb') as f:
+                self._append_page(f, DataPage([record], -1))
+            self.build_index()
+            return
+        target_page = self._find_primary_page_for_key(record.id)
+        if target_page is None:
+            with open(self.filename, 'wb') as f:
+                self._append_page(f, DataPage([record], -1))
+            self.build_index()
+            return
+        with open(self.filename, 'r+b') as f:
+            page = self._read_page(f, target_page)
+            if page.has_space():
+                ids = [r.id for r in page.records]
+                pos = 0
+                for i, v in enumerate(ids):
+                    if record.id <= v:
+                        pos = i
+                        break
+                else:
+                    pos = len(ids)
+                page.records.insert(pos, record)
+                self._write_page(f, target_page, page)
+                return
+            prev = target_page
+            curr = page.next_page
+            while curr != -1:
+                p = self._read_page(f, curr)
+                if p.has_space():
+                    p.records.append(record)
+                    self._write_page(f, curr, p)
+                    return
+                prev = curr
+                curr = p.next_page
+            new_page = DataPage([record], -1)
+            new_no = self._append_page(f, new_page)
+            if prev == target_page:
+                page.next_page = new_no
+                self._write_page(f, target_page, page)
+            else:
+                last = self._read_page(f, prev)
+                last.next_page = new_no
+                self._write_page(f, prev, last)
 
     def search(self, key):
-        index_block = None
-        for max_key, i_index in self.index_root:
-            if key <= max_key:
-                index_block = i_index
-                break
-        if index_block is None:
-            return None
-        page_index = None
-        for max_key, p_index in self.index_level_1[index_block:index_block + INDEX_BLOCK_FACTOR]:
-            if key <= max_key:
-                page_index = p_index
-                break
-        if page_index is None:
-            return None
-        page = self.data_pages[page_index]
-        for r in page.records:
-            if r.id == key:
-                return r
-        return None
-
-    def add(self, record):
-        for i, (max_key, page_index) in enumerate(self.index_level_1):
-            if record.id <= max_key:
-                page = self.data_pages[page_index]
-                if not page.is_full():
-                    page.add_record(record)
-                else:
-                    overflow_page = DataPage([record])
-                    page.overflow = len(self.data_pages)
-                    self.data_pages.append(overflow_page)
-                break
-        else:
-            new_page = DataPage([record])
-            self.data_pages.append(new_page)
-            self.index_level_1.append((record.id, len(self.data_pages) - 1))
-        self.save_to_disk()
-
-    def range_search(self, start_key, end_key):
+        self._load_index()
+        if not os.path.exists(self.filename):
+            return []
+        page_no = self._find_primary_page_for_key(key)
+        if page_no is None:
+            return []
         result = []
-        for page in self.data_pages:
-            for r in page.records:
-                if start_key <= r.id <= end_key:
+        with open(self.filename, 'rb') as f:
+            p = self._read_page(f, page_no)
+            for r in p.records:
+                if r.id == key:
                     result.append(r)
+                elif r.id > key:
+                    break
+            ov = p.next_page
+            while ov != -1:
+                op = self._read_page(f, ov)
+                for r in op.records:
+                    if r.id == key:
+                        result.append(r)
+                ov = op.next_page
         return result
 
+    def range_search(self, start, end):
+        self._load_index()
+        if not os.path.exists(self.filename):
+            return []
+        if start > end:
+            start, end = end, start
+        res = []
+        start_page = self._find_primary_page_for_key(start)
+        if start_page is None:
+            return res
+        total = self.primary_page_count
+        with open(self.filename, 'rb') as f:
+            p = start_page
+            while p < total:
+                page = self._read_page(f, p)
+                for r in page.records:
+                    if start <= r.id <= end:
+                        res.append(r)
+                    elif r.id > end:
+                        return res
+                ov = page.next_page
+                while ov != -1:
+                    op = self._read_page(f, ov)
+                    for r in op.records:
+                        if start <= r.id <= end:
+                            res.append(r)
+                    ov = op.next_page
+                p += 1
+        return res
+
+    def remove(self, key):
+        self._load_index()
+        if not os.path.exists(self.filename):
+            return 0
+        removed = 0
+        page_no = self._find_primary_page_for_key(key)
+        if page_no is None:
+            return 0
+        with open(self.filename, 'r+b') as f:
+            page = self._read_page(f, page_no)
+            before = len(page.records)
+            page.records = [r for r in page.records if r.id != key]
+            removed += before - len(page.records)
+            self._write_page(f, page_no, page)
+            ov = page.next_page
+            while ov != -1:
+                op = self._read_page(f, ov)
+                before = len(op.records)
+                op.records = [r for r in op.records if r.id != key]
+                removed += before - len(op.records)
+                self._write_page(f, ov, op)
+                ov = op.next_page
+        return removed
+
+    def scanAll(self):
+        if not os.path.exists(self.filename):
+            print("(Archivo vacío)")
+            return
+        total = self._total_pages()
+        with open(self.filename, 'rb') as f:
+            for pno in range(total):
+                page = self._read_page(f, pno)
+                print(f"Página {pno} [size={len(page.records)}, next={page.next_page}]")
+                for i, r in enumerate(page.records, 1):
+                    print(f"  - {r}")
+        self._load_index()
+        print("==== LEAF ====")
+        for mx, pno in self.leaf:
+            print(f"  ({mx} -> {pno})")
+        print("==== ROOT ====")
+        for mx, start in self.root:
+            print(f"  ({mx} -> {start})")
+
 if __name__ == "__main__":
-    records = [Record(id_) for id_ in
-               [101, 105, 108, 110, 112, 115, 118, 120, 122, 125,
-                128, 130, 133, 136, 139, 142, 145, 147, 149, 150]]
-
-    isam = ISAMFile()
-    isam.build_from_records(records)
-
-    print("Índice raíz:", isam.index_root)
-    print("Índice nivel 1:", isam.index_level_1)
-    print("Páginas de datos:")
-    for i, p in enumerate(isam.data_pages):
-        print(f"  Página {i}: {p}")
-
-    print("\nBuscar id=115 →", isam.search(115))
-    print("Buscar rango [110, 130] →", isam.range_search(110, 130))
-
-    print("\nInsertando id=111...")
+    for fn in ["isam_data.dat", "isam_data.dat_idx"]:
+        try: os.remove(fn)
+        except: pass
+    isam = ISAMFile("isam_data.dat")
+    base = [Record(i) for i in [101, 105, 108, 110, 112, 115, 118, 120, 122, 125]]
+    isam.build_from_records(base)
+    print("== Estructura inicial ==")
+    isam.scanAll()
+    print("\nInsertando nuevos registros...")
     isam.add(Record(111))
-    for i, p in enumerate(isam.data_pages):
-        print(f"  Página {i}: {p}")
+    isam.add(Record(113))
+    isam.add(Record(114))
+    isam.scanAll()
+    print("\nsearch(112) ->", isam.search(112))
+    print("range_search [108,115] ->", isam.range_search(108,115))
+    print("\nremove(111) -> eliminados:", isam.remove(111))
+    isam.scanAll()
